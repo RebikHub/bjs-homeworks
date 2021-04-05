6147f2716ef8c16c020efaf50a9fc239d234fbc9
Alexandr Rebikov <rebikovlk@mail.ru>
2021-04-05 12:11:51 +0700

    Задача 2 и правки Задачи 1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -5,10 +5,10 @@ function getResult(a,b,c){
     let D = Math.pow(b, 2) - 4 * a * c;
 
     if (D > 0) {
-        x.push((- b + D) / (2 * a));
-        x.push((- b - D) / (2 * a));
+        x.push((- b + Math.sqrt(D)) / (2 * a));
+        x.push((- b - Math.sqrt(D)) / (2 * a));
         return x;
-    } else if (D = 0) {
+    } else if (D === 0) {
         x.push((- b) / (2 * a));
         return x;
     };
@@ -17,8 +17,23 @@ function getResult(a,b,c){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    console.log(marks.length);
+    let sumMarks = 0;
+    let averageMark;
+
+    if (marks.length === 0) {
+        return averageMark = 0;
+    } else if (marks.length > 5) {
+        let overMarks = marks.length - 5;
+        marks.splice(5, overMarks);
+    };
+
+    for (let i = 0; i < marks.length; i++) {
+        sumMarks = sumMarks + marks[i];
+        averageMark = sumMarks / marks.length;
+    };
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
