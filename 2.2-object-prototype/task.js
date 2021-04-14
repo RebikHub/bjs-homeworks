91c20303c739ab2ff3bd9a057e61e9a1f33af771
Alexandr Rebikov <rebikovlk@mail.ru>
2021-04-14 15:28:35 +0700

    Задача №1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,26 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+// Задача №1
+
+String.prototype.isPalindrome = function() {
+    let concWords = '';
+
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] !== ' ') {
+            concWords += this[i];
+        };
+    };
+    
+    let caseConcWords = concWords.toLowerCase();
+
+    if (caseConcWords === caseConcWords.split('').reverse().join('')) {
+        return true;
+    };
+    return false;
+};
+
+let str = 'А роза упала на лапу Азора';
+console.log(str.isPalindrome());
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
