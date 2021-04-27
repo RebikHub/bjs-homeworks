09336f9a5fc7d1559680287c58529978164b398a
Alexandr Rebikov <rebikovlk@mail.ru>
2021-04-27 16:49:11 +0700

    Задача №2 + testCase

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -49,38 +49,47 @@ function getValuestCountToSumValues(arr, sum) {
 function sleep(milliseconds) {
     let e = new Date().getTime() + milliseconds;
     while (new Date().getTime() <= e) {}
-}
+};
 
 function sum(...args) {
     sleep(100);
     return args.reduce((sum, arg) => sum + arg, 0);
-}
+};
 
 function compareArrays(arr1, arr2) {
     if (arr1.length === arr2.length) {
         return arr1.every((item, index) => item === arr2[index]);
     };
     return false;
-}
+};
 
 function memorize(fn, limit) {
     const memory = [];
-    fn = function sum(...args) {
-        let obj = {
-            args: [],
-        };
-        obj.args = Array.from(arguments);
-        obj.result = args.reduce((sum, arg) => sum + arg, 0);
-        if (memory.some((item) => compareArrays(item.args, Array.from(arguments)))) {
-        memory.push(obj);
-        } else {
-            return obj.result;
-        }
-        sleep(100);
-        return args.reduce((sum, arg) => sum + arg, 0);
-    };
     if (memory.length > limit) {
         memory.shift();
     };
-    return fn;
-}
+    return function () {
+        if (memory.find((item) => compareArrays(item.args, Array.from(arguments)))) {
+            return memory.find((item) => compareArrays(item.args, Array.from(arguments))).result;
+        };
+        let obj = {};
+        obj.args = Array.from(arguments);
+        obj.result = fn(...arguments);
+        memory.push(obj);
+        return obj.result;
+    };
+};
+
+function testCase(testFunction, nameTimer) {
+    const arr = [[1,2,3], [1,2], [2,3,4], [5,2,4], [6,7,8,2], [9,1,4,5], [1,2], [5,2,4]];
+    console.time(nameTimer);
+    for (let i = 0; i < arr.length; i++) {
+        arr.forEach(element => testFunction(...element) * i);
+    };
+    return console.timeEnd(nameTimer);
+};
+
+const mSum = memorize(sum, 5);
+
+testCase(sum, 'sum'); // sum: 6465.000732421875 ms
+testCase(mSum, 'mSum'); // mSum: 606.396728515625 ms
